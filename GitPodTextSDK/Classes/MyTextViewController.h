84c7a2114a7c26c3c8acd2ec38d4ea5d0079069d
linmingzhi987 <linmingzhi987@126.com>
2016-12-22 11:36:31 +0800

    12

---
+++ GitPodTextSDK/Classes/MyTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTextViewController.h
+//  MyTextNameSDK
+//
+//  Created by 悦厚 on 22/12/16.
+//  Copyright © 2016年 悦厚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyTextViewController : NSObject
+
+@end
---
+++ GitPodTextSDK/Classes/MyTextViewController.m
@@ -0,0 +1,13 @@
+//
+//  MyTextViewController.m
+//  MyTextNameSDK
+//
+//  Created by 悦厚 on 22/12/16.
+//  Copyright © 2016年 悦厚. All rights reserved.
+//
+
+#import "MyTextViewController.h"
+
+@implementation MyTextViewController
+
+@end
